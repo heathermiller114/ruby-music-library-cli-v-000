2e7af73f5f1d518ab4ab136a83f07f97d735e0f8
Heather Miller <heather.miller114@gmail.com>
2018-10-07 23:03:01 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 class MusicLibraryController
  
   attr_reader :path
@@ -25,6 +25,7 @@ class MusicLibraryController
   
   def list_songs
     Song.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
+      binding.pry
       puts "#{i}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
     end
   end
