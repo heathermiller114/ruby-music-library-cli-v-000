dda01217b422e5d59663e8e4ff5ffaaa9d503932
Heather Miller <heather.miller114@gmail.com>
2018-10-07 23:01:42 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -24,8 +24,8 @@ class MusicLibraryController
   end
   
   def list_songs
-    Song.all.sort_by(&:name).each.with_index(1) do |song, index|
-      puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+    Song.all.sort_by(&:name).each.with_index(1) do |song, ind|
+      puts "#{ind}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     end
   end
 end
