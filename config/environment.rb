ca322cd555fd9f686a6e2172fd67cc6c8b2a95cd
Heather Miller <heather.miller114@gmail.com>
2018-10-07 22:52:45 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,21 @@ require 'bundler'
 Bundler.require
 
 module Concerns
+  module Findable
+    
+    def find_by_name(song_name)
+      self.all.find {|song| song.name == song_name}
+    end
+    
+    def find_or_create_by_name(song_name)
+      if find_by_name(song_name) == nil
+        create(song_name)
+      else
+        find_by_name(song_name)
+      end
+    end
+    
+  end
 end
 
 require_all 'lib'
