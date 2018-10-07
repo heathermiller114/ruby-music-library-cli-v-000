c26088e229c997c2ba2cd82523e2e35420f5cd70
Heather Miller <heather.miller114@gmail.com>
2018-10-07 23:56:15 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -30,5 +30,7 @@ class MusicLibraryController
   end
   
   def list_artists
-    
+    Artist.all.sort {|a, b| a.name <=> b.name}.each.with_index(1) do |artist, index|
+      puts "#{index}."
+    end
 end
