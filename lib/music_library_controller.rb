9c1cc48152de9c940c281ca24105b1869f30c5d4
Heather Miller <heather.miller114@gmail.com>
2018-10-08 00:15:21 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -56,7 +56,7 @@ class MusicLibraryController
     input = gets.strip
     if genre = Genre.find_by_name(input)
       genre.songs.sort{|a, b| a.name <=> b.name}.each.with_index(1) do |song, index|
-        puts "#{index}. #"
+        puts "#{index}. #{song.artist.name} - #{song.name}"
       end
     end
   end
