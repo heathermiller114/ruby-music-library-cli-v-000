661820a2dfa95d192b29b82942bc49e19ef55748
Heather Miller <heather.miller114@gmail.com>
2018-10-08 00:34:19 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -20,6 +20,11 @@ class MusicLibraryController
       puts "What would you like to do?"
       
       input = gets.strip
+      
+      case input
+      when 'list songs'
+        list_songs
+      end
     end
   end
   
