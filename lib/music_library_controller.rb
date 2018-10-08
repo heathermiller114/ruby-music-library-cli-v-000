81f430cb3fcd3f6827c232d876b12bdefe731989
Heather Miller <heather.miller114@gmail.com>
2018-10-08 00:05:41 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -44,6 +44,7 @@ class MusicLibraryController
   def list_songs_by_artist
     puts "Please enter the name of an artist:"
     input = gets.strip
+    if Artist.find_by_name(input)
     
   end
 end
